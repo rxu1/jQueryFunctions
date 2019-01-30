6c5362d6e00c7e779a276f2645792de6c9738f16
Raymond Xu <raymondxu92@gmail.com>
2019-01-30 14:08:24 -0800

    jQuery

---
+++ script.js
@@ -0,0 +1,10 @@
+$(".subcontents").click(function(){
+    $(this).css("background", "purple");
+});
+$("#navigation").click(function(){
+    $(this).css("background", "url('hero_chara_mario_update_pc.png')");
+
+})
+
+$("url'hero_chara_mario_update_pc.png'").width(200); 
+$("url'hero_chara_mario_update_pc.png'").height(300);
