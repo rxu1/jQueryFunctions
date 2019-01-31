12301c69781dcbdcc6c7646da64cdb3395014bbc
Raymond Xu <raymondxu92@gmail.com>
2019-01-30 16:10:01 -0800

    jquery

--- script.js
+++ script.js
@@ -2,9 +2,38 @@ $(".subcontents").click(function(){
     $(this).css("background", "purple");
 });
 $("#navigation").click(function(){
-    $(this).css("background", "url('hero_chara_mario_update_pc.png')");
-
+    $(this).css({
+        "background": "url('hero_chara_mario_update_pc.png')",
+        "background-size": "contain", 
+    });
+})
+$("#header").click(function (){
+    $(this).hide();
+});
+$("#button1").click(function(){
+    $(this).slideDown();
+})
+$("#button3").hover(function(){
+    $("#main_content").fadeIn();
+    $(this).slideUp();
 })
 
-$("url'hero_chara_mario_update_pc.png'").width(200); 
-$("url'hero_chara_mario_update_pc.png'").height(300);
+$(".btn").click(function(){
+    $("#main_content").fadeOut();
+})
+$("#wrapper").hover(function(){
+    $(this).html();
+    })
+
+    $( "#button2" ).click(function() {
+      $(".mario").animate({
+        opacity: .5,
+        right: "+=50",
+        height: "-=123"
+      }, 10000, function() {
+      });
+    });
+    $("#text").keydown(function(){
+        var value=$(this).val();
+        $("p").text(value);
+    })
